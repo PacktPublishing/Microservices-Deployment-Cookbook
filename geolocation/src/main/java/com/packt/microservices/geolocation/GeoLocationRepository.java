a7958a084d1ce945dd14b4691d1470f1fa33e5f6
Vikram Murugesan <mvikramramesh@gmail.com>
2017-01-04 20:29:44 -0600

    Chapter 03 - Initial Commit

--- geolocation/src/main/java/com/packt/microservices/geolocation/GeoLocationRepository.java
+++ geolocation/src/main/java/com/packt/microservices/geolocation/GeoLocationRepository.java
@@ -1,21 +1,34 @@
 package com.packt.microservices.geolocation;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Repository
 public class GeoLocationRepository {
 
-  private List<GeoLocation> geolocations = new ArrayList<GeoLocation>();
-  
-  public void addGeoLocation(GeoLocation geolocation) {
-    geolocations.add(geolocation);
-  }
-  
-  public List<GeoLocation> getGeoLocations() {
-    return Collections.unmodifiableList(geolocations);
-  }
+	private List<GeoLocation> geolocations = new ArrayList<GeoLocation>();
+	
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+	private static final String DATA_FILES_DIR = System.getenv("GEOLOCATION_DATA_FILES_DIR") != null 
+			? System.getenv("GEOLOCATION_DATA_FILES_DIR") : "/opt/packt/geolocation/data"; 
+	
+	public void addGeoLocation(GeoLocation geolocation) {
+		geolocations.add(geolocation);
+		
+		try {
+			MAPPER.writeValue(new File(DATA_FILES_DIR + "/user" + geolocation.getUserId() + "_t" + geolocation.getTimestamp()), geolocation);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<GeoLocation> getGeoLocations() {
+		return Collections.unmodifiableList(geolocations);
+	}
 }
