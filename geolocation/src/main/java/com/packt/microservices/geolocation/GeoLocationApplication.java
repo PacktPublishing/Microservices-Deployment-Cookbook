0e8e7e8bc19a97682f5e2d4dbd06a9f265d96a3d
Vikram Murugesan <mvikramramesh@gmail.com>
2017-01-13 03:19:23 -0600

    Chapter 06 - Initial Commit

--- geolocation/src/main/java/com/packt/microservices/geolocation/GeoLocationApplication.java
+++ geolocation/src/main/java/com/packt/microservices/geolocation/GeoLocationApplication.java
@@ -2,10 +2,9 @@ package com.packt.microservices.geolocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class GeoLocationApplication {
 
   public static void main(String[] args) {
